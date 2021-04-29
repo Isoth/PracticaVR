2707b513e07010a3cd2ac8d16fea920e88ba6227
Javier Hernández <javier.sk.99@gmail.com>
2021-04-29 16:55:03 +0200

    Algunos scripts y tal :3

---
+++ VR/Assets/Scripts/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Camera : MonoBehaviour
+{
+
+    public float camSens = 0.50f; //Sensibilidad con el ratón
+    Vector3 lastMouse = new Vector3(255, 255, 255);
+    [SerializeField] GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!XRSettings.enabled)
+        {
+            CameraPosition();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootABullet();
+        }
+    }
+
+    void CameraPosition()
+    {
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+    }
+
+    void ShootABullet()
+    {
+        GameObject bulletInstance = Instantiate(bullet, transform.position, transform.rotation);
+        Bullet bh = bulletInstance.GetComponent<Bullet>();
+        if (bh != null)
+        {
+            bh.Shoot(transform.forward);
+        }
+        else
+        {
+            Debug.Log("La Bullet no tiene Script BulletHandler");
+            Destroy(bulletInstance, 3f);
+        }
+    }
+}
---
+++ VR/Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] spawnPoints;
+    [SerializeField]
+    private GameObject[] typesOfPeople;
+
+    public float minSpawnTime = 1f;
+    public float maxSpawnTime = 10f;
+
+    private bool stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnPeople());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnPeople()
+    {
+        while (stop == false)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            Instantiate(typesOfPeople[Random.Range(0, typesOfPeople.Length)], spawnPoints[Random.Range(0, spawnPoints.Length)].transform);
+        }
+    }
+
+    public void Stop()
+    {
+        stop = true;
+    }
+}
